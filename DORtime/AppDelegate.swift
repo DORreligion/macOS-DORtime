4d5add0a7092dec72fcc646e16a45520d478da0d
Lukas Schramm <schramm98@icloud.com>
2015-10-04 11:18:41 +0200

    Initial Commit

---
+++ DORtime/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DORtime
+//
+//  Created by Lukas Schramm on 04.10.15.
+//  Copyright © 2015 Lukas Schramm. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
